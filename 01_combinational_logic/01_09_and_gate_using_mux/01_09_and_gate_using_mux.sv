f917152ed587f9503af4dca928f4eb5f99342dee
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-12 17:11:41 +0300

    feat: 01_09_and_gate_using_mux.sv done

--- 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
+++ 01_combinational_logic/01_09_and_gate_using_mux/01_09_and_gate_using_mux.sv
@@ -28,5 +28,9 @@ module and_gate_using_mux
   // Implement and gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
 
+  wire temp;
+
+  mux mux1 (.d0(1'b0), .d1(b), .sel(a), .y(temp));
+  mux mux2 (.d0(1'b0), .d1(temp), .sel(1'b1), .y(o));
 
 endmodule
