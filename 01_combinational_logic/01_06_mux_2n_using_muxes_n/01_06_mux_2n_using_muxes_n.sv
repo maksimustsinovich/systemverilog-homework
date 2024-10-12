df0a4c7cf5c69e120de13cf27dfda924f699ade8
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-12 17:07:47 +0300

    feat: 01_06_mux_2n_using_muxes_n.sv done

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -27,5 +27,10 @@ module mux_4_1
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
 
+  wire [3:0] temp1, temp2;
+
+  mux_2_1 mux1 (.d0(d0), .d1(d1), .sel(sel[0]), .y(temp1));
+  mux_2_1 mux2 (.d0(d2), .d1(d3), .sel(sel[0]), .y(temp2));
+  mux_2_1 mux3 (.d0(temp1), .d1(temp2), .sel(sel[1]), .y(y));
 
 endmodule
