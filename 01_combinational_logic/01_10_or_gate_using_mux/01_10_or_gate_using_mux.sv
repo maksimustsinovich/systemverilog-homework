1a9a8ae9d69f6ccceb677372cf73746789b6ad90
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-12 18:13:46 +0300

    feat: 01_10_or_gate_using_mux.sv done

--- 01_combinational_logic/01_10_or_gate_using_mux/01_10_or_gate_using_mux.sv
+++ 01_combinational_logic/01_10_or_gate_using_mux/01_10_or_gate_using_mux.sv
@@ -29,5 +29,9 @@ module or_gate_using_mux
   // Implement or gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
 
+  wire mux0_o;
+
+  mux mux0(.d0(1'b0), .d1(1'b1), .sel(a), .y(mux0_o));
+  mux mux1(.d0(mux0_o), .d1(1'b1), .sel(b), .y(o));
 
 endmodule
