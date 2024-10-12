d93d74d3ab16f88f60eddb293fa52b3e688fdbef
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-12 16:56:53 +0300

    feat: 01_03_mux_case.sv done

--- 01_combinational_logic/01_03_mux_case/01_03_mux_case.sv
+++ 01_combinational_logic/01_03_mux_case/01_03_mux_case.sv
@@ -32,5 +32,12 @@ module mux_4_1
   // Using code for mux_2_1 as an example,
   // write code for 4:1 mux using the "case" statement
 
+  always_comb
+    case(sel)
+      2'b00: y = d0;
+      2'b01: y = d1;
+      2'b10: y = d2;
+      default: y = d3;
+    endcase
 
 endmodule
