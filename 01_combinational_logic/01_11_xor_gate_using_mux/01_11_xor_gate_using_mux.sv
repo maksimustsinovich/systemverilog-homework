55cf00d9358f7b5650e6cbca1fba3eab3f105d5b
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-12 18:24:01 +0300

    feat: 01_11_xor_gate_using_mux.sv done

--- 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
+++ 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
@@ -28,5 +28,9 @@ module xor_gate_using_mux
   // Implement xor gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
 
+  wire mux0_o;
+
+  mux mux0(.d0(1'b1), .d1(1'b0), .sel(b), .y(mux0_o));
+  mux mux1(.d0(b), .d1(mux0_o), .sel(a), .y(o));
 
 endmodule
