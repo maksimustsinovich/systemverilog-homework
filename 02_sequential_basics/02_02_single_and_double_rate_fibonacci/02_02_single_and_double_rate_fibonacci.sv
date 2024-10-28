997117564e72ffba0f6b67baadc8800035dc0170
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-29 01:19:36 +0300

    02_02_single_and_double_rate_fibonacci.sv

--- 02_sequential_basics/02_02_single_and_double_rate_fibonacci/02_02_single_and_double_rate_fibonacci.sv
+++ 02_sequential_basics/02_02_single_and_double_rate_fibonacci/02_02_single_and_double_rate_fibonacci.sv
@@ -34,5 +34,19 @@ module fibonacci_2
   // Task:
   // Implement a module that generates two fibonacci numbers per cycle
 
+  logic [15:0] a, b;
+
+  always_ff @ (posedge clk)
+    if (rst)
+    begin
+        { a, b } <= { 16'd1, 16'd1 };
+        { num, num2 } <= { a, b };
+    end
+    else
+    begin
+        { a, b } = { b, a + b };
+        { a, b } = { b, a + b };
+        { num, num2 } <= { a, b };
+    end
 
 endmodule
