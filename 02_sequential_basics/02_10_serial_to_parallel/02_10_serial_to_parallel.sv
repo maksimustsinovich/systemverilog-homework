fc6e2c46a8914a8e97d79d344669407bb147cfcf
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-10-30 23:45:21 +0300

    02_10_serial_to_parallel.sv

--- 02_sequential_basics/02_10_serial_to_parallel/02_10_serial_to_parallel.sv
+++ 02_sequential_basics/02_10_serial_to_parallel/02_10_serial_to_parallel.sv
@@ -26,5 +26,37 @@ module serial_to_parallel
     // Note:
     // Check the waveform diagram in the README for better understanding.
 
+    logic [3:0] counter;
+    logic [width - 1:0] temp_data;
+    logic temp_valid;
+
+    //always_comb begin
+    //    if (temp_valid) begin
+    //        temp_data = 0;
+    //        temp_valid = 0;
+    //    end
+    //end
+
+    always_ff @ (posedge clk) begin
+        if (rst) begin
+            counter <= 0;
+            temp_data <= 0;
+            temp_valid <= 0;
+        end
+        else begin
+            temp_valid <= 0;
+            if (serial_valid) begin
+                temp_data <= (temp_data >> 1) + (serial_data << (width - 1));
+                counter++;
+            end
+            if (counter == width) begin
+                counter <= 0;
+                temp_valid <= 1;
+            end
+        end
+    end
+
+    assign parallel_data = temp_data;
+    assign parallel_valid = temp_valid;
 
 endmodule
